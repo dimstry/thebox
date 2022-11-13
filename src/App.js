e8561236630f4f488fd59c77b131af0fc60374fc
dimstry <dimastriana03@gmail.com>
2022-11-13 13:24:59 +0700

    add services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Reasons from "./components/Reasons";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Reasons />
       <About />
+      <Services />
     </>
   );
 }
